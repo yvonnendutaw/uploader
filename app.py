3f2cbf6fbc8155d6653ec90294e25b640b1991b6
yvonnendutaw <yvonita7@gmail.com>
2016-06-12 00:27:11 +0300

    added files

--- app.py
+++ app.py
@@ -54,7 +54,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug = True)
